48a58ff10738df4aeffea66ac88a0c6437437a5e
mc3waller <59793457+mc3waller@users.noreply.github.com>
2021-03-03 14:17:50 -0800

    Adds minor fixes to views and redirect files

--- PythonClub/pythonclub/urls.py
+++ PythonClub/pythonclub/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('club/', include('tech.urls')),
+    path('club/', include('club.urls')),
 ]
