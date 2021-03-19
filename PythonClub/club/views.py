180b70751bd9972592e68eb7a4845d8703a2de48
mc3waller <59793457+mc3waller@users.noreply.github.com>
2021-03-19 03:26:36 -0700

    Adds initial code for resources view

--- PythonClub/club/views.py
+++ PythonClub/club/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'club/index.html')
+
+def resources(request):
+    resourceName = Resource.objects.all()
+    return render(request, 'club/resources.html', {'resourceName': resourceName})
