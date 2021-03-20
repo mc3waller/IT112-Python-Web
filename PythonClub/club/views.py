35b1fd752cf555927854f3eab5e1c836291c1329
mc3waller <59793457+mc3waller@users.noreply.github.com>
2021-03-20 01:48:07 -0700

    Adds view information for meeting details

--- PythonClub/club/views.py
+++ PythonClub/club/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Meeting, MeetingMinutes, Resource, ResourceType, Event
 
 # Create your views here.
@@ -7,4 +7,12 @@ def index(request):
 
 def resources(request):
     resource_list = Resource.objects.all()
-    return render(request, 'club/resources.html', {'resource_list': resource_list})
+    return render(request, 'club/resources.html', {'resource_list' : resource_list})
+
+def meetings(request):
+    meeting_list = Meeting.objects.all()
+    return render(request, 'club/meetings.html', {'meeting_list' : meeting_list})
+
+def meetingDetails(request, id):
+    meeting = get_object_or_404(Meeting, pk=id)
+    return render(request, 'club/details.html', {'meeting' : meeting})
