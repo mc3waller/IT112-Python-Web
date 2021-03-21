8fb1ff3f7811a63a7275c986fb0c4f13b35374a7
mc3waller <59793457+mc3waller@users.noreply.github.com>
2021-03-20 18:25:26 -0700

    Adds further model and incomplete view testing

--- PythonClub/club/tests.py
+++ PythonClub/club/tests.py
@@ -1,8 +1,14 @@
 from django.test import TestCase
+from .views import index, resources, meetings, meetingDetails
+from django.urls import reverse
 from django.contrib.auth.models import User
 from .models import Meeting, MeetingMinutes, ResourceType, Resource, Event
 
 # Create your tests here.
+
+# ==============================
+# MODEL STRING & TABLENAME TEST
+# ==============================
 
 class MeetingTest(TestCase):
     def setUp(self):
@@ -16,7 +22,19 @@ class MeetingTest(TestCase):
 
 
 
-class Resource(TestCase):
+class ResourceTypeTest(TestCase):
+    def setUp(self):
+        self.type = ResourceType(typeName = 'Book')
+
+    def test_resourcetypestring(self):
+        self.assertEqual(str(self.type), 'Book')
+
+    def test_tablename(self):
+        self.assertEqual(str(ResourceType._meta.db_table), 'resourcetype')
+
+
+
+class ResourceTest(TestCase):
     def setUp(self):
         self.resource = Resource(resourceName = 'Python for Kids')
 
@@ -25,3 +43,45 @@ class Resource(TestCase):
 
     def test_tablename(self):
         self.assertEqual(str(Resource._meta.db_table), 'resource')
+
+
+
+class EventTest(TestCase):
+    def setUp(self):
+        self.event = Event(eventTitle = 'Job Fair')
+
+    def eventstring(self):
+        self.assertEqual(str(self.event), 'Job Fair')
+
+    def test_tablename(self):
+        self.assertEqual(str(Event._meta.db_table), 'event')
+
+
+# ==============================
+# VIEW TEST
+# ==============================
+
+class IndexViewTest(TestCase):
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+
+class ResourcesViewTest(TestCase):
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('resources'))
+        self.assertEqual(response.status_code, 200)
+
+
+class MeetingsViewTest(TestCase):
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('meetings'))
+        self.assertEqual(response.status_code, 200)
+
+class MeetingDetailsViewTest(TestCase):
+    def setUp(self):
+        self.meeting = Meeting(meetingTitle = "First", meetingDate='2021-01-01', meetingTime='00:00:00', location="Seattle", agenda='n/a')
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('details', args=(self.meeting.id,)))
+        self.assertEqual(response.status_code, 200)
