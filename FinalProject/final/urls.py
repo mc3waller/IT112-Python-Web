92aeab9e10b617cbed2ad1cacdd6620425a74495
mc3waller <59793457+mc3waller@users.noreply.github.com>
2021-03-22 04:11:36 -0700

    Adds view with first item list

--- FinalProject/final/urls.py
+++ FinalProject/final/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.index, name = 'index'),
+    path('games/', views.games, name = 'games')
 ]
--- FinalProject/final/views.py
+++ FinalProject/final/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Game, Genre, Review
 
 # Create your views here.
 
 def index(request):
     return render(request, 'final/index.html')
+
+def games(request):
+    game_list = Game.objects.all()
+    return render(request, 'final/games.html', {'game_list': game_list})
