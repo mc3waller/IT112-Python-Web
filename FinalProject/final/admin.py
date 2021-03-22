a3462c91c95bc620e6007bb34674569bac3f985e
mc3waller <59793457+mc3waller@users.noreply.github.com>
2021-03-22 03:52:37 -0700

    Adds initial models and admin data

--- FinalProject/final/admin.py
+++ FinalProject/final/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Genre, Game, Review
 
 # Register your models here.
+
+admin.site.register(Genre)
+admin.site.register(Game)
+admin.site.register(Review)
--- FinalProject/final/models.py
+++ FinalProject/final/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# REMINDER: ID's are auto-generated outside of the code found here
+
+class Genre(models.Model):
+    GenreName = models.CharField(max_length=255)
+    GenreDescription = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.GenreName
+    
+    class Meta:
+        db_table = 'genre'
+
+class Game(models.Model):
+    GameTitle = models.CharField(max_length=255)
+    Developer = models.CharField(max_length=255)
+    Genre = models.ForeignKey(Genre, on_delete = models.DO_NOTHING)
+    ReleaseDate = models.DateField()
+    AverageHours = models.IntegerField()
+    GameUrl = models.URLField()
+    Description = models.TextField()
+    User = models.ForeignKey(User, on_delete = models.DO_NOTHING)
+
+    def __str__(self):
+        return self.GameTitle
+
+    class Meta:
+        db_table = 'game'
+
+class Review(models.Model):
+    ReviewTitle = models.CharField(max_length=255)
+    GameTitle = models.ForeignKey(Game, on_delete = models.CASCADE)
+    ReviewDate = models.DateField()
+    ReviewContent = models.TextField()
+    User = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.ReviewTitle
+
+    class Meta:
+        db_table = 'review'
